725f9fe094a15a71c02750524e3bf068bffd70cb
Steve NGOYA <maximusdevcode@gmail.com>
2025-04-17 17:42:39 +0100

    new file:   images/20220609_171629_transcpr.jpg 	new file:   images/20230705_133124.jpg 	new file:   images/464690235_1586243078951863_456041130619487436_n.jpg 	new file:   images/AMyImage2.jpg 	new file:   "images/Capture d'\303\251cran 2024-12-24 125401.png" 	new file:   "images/Capture d'\303\251cran 2024-12-27 105308.png" 	new file:   "images/Capture d'\303\251cran 2025-04-06 221800.png" 	new file:   images/IMG_1757.JPG 	new file:   images/IMG_1764.JPG 	new file:   images/IMG_20220107_113428_103.jpg 	new file:   images/MyImage.jpg 	new file:   images/MyImage1.jpg 	new file:   images/Steve-Profile.jpeg 	new file:   images/WhatsApp Image 2025-03-22 at 18.37.51_9b3432de.jpg 	new file:   images/bg.jpeg 	new file:   images/gal1.jpeg 	new file:   images/gal2.jpeg 	new file:   images/gal3.jpeg 	new file:   images/hearth.png 	new file:   images/img1.jpg 	new file:   images/img2.jpg 	new file:   images/img3.jpg 	new file:   images/img4.jpg 	new file:   images/shirt.png 	new file:   images/style.css 	new file:   images/suit.png 	new file:   images/t-shirt.png 	new file:   index.css 	new file:   index.html 	new file:   index.js 	new file:   jquery-3.6.0.min.js 	new file:   remixicon.css 	new file:   scrollreveal.min.js 	new file:   show3D.html 	new file:   styles.css 	new file:   swiper-bundle.min.css 	new file:   swiper-bundle.min.js 	new file:   tailwind.min.css

---
+++ index.js
@@ -0,0 +1,161 @@
+$(document).ready(function() {
+    // var scale = 1;
+    // var increment = 0.0004;
+    // var interval = 50; // 16ms = approximately 60fps
+
+    //       setInterval(function() {
+    //         scale += increment;
+    //         if (scale >= 1.1) {
+    //           scale = 1;
+    //         }
+    //         $('.container').css('background-size', `${scale * 100}% ${scale * 100}%`);
+    //       }, interval);
+
+        $('.account-menu').hide();
+
+        // Toggle account menu on button click
+        $('.toggle-account-menu').click(function () {
+            $('.account-menu').fadeToggle(); // Toggle visibility of the account menu with fade animation
+        });
+
+
+        $(document).click(function (event) {
+            if (!$(event.target).closest('.toggle-account-menu, .account-menu, .toggle-menu').length) {
+                $('.account-menu').slideUp(500); 
+                $('.navbar').removeClass('active');
+            }
+        });
+
+        $('.toggle-menu').click(()=>{
+            $('.navbar').addClass('active')
+            $('.navbar').show();
+        })
+
+        $('.container').scroll(()=>{
+            $('.navbar').removeClass('active')
+        })
+
+        $('.download_label').on('click', function(e) {
+            e.preventDefault(); // Prevent default action
+            
+            const imageUrl = $(this).prev('.card_img').attr('src'); // Get the corresponding image source
+            const fileName = imageUrl.split('/').pop(); // Extract the file name
+            
+            // Create a temporary link element
+            const link = document.createElement('a');
+            link.href = imageUrl; // Set the href to the image URL
+            link.download = fileName; // Set the download attribute to the file name
+            document.body.appendChild(link); // Append link to the document
+            link.click(); // Trigger the download
+            document.body.removeChild(link); // Remove the link from the document
+        });
+
+ });
+
+
+
+    let section = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('.menu ul a');
+
+    document.querySelector('.container').onscroll = () => {
+        section.forEach(sec => {
+            let top = window.scrollY;
+            let height = sec.offsetHeight;
+            let offset = sec.offsetTop +150; // This adjusts for a possible fixed header
+            let id = sec.getAttribute('id');
+
+            // Corrected the conditional operator
+            if (top >= offset && top < offset + height) { 
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                });
+                // Add active class to the corresponding nav link
+                document.querySelector(`.menu ul a[href*="${id}"]`).classList.add('active');                
+            }
+        });
+    };
+
+
+
+    // const scrollRevealOption = {
+    //   distance: '50px',
+    //   duration: 1000,
+    //   origin: 'bottom',
+    //   viewFactor: 0.25
+    // };
+    
+    // ScrollReveal().reveal('.hero_image img', {
+    //   ...scrollRevealOption,
+    //   origin: 'right',
+    // });
+   
+    ScrollReveal().reveal('.hero_image_card', {
+        duration:1000,
+        interval:500,
+        delay:500
+    });
+
+
+    
+
+
+  
+    
+
+
+    let items = document.querySelectorAll('.item');
+    let action = document.getElementById('action');
+
+
+    items.forEach(item => {
+        item.addEventListener('click', function(e){
+            if( this.classList.contains('active') || e.target.classList.contains('fa-chevron-down')){
+                return;
+            }
+            items.forEach(remove_active => {
+                remove_active.classList.remove('active');
+            });
+            this.classList.add('active');
+            document.documentElement.style.setProperty('--height-begin', action.offsetHeight + 'px');
+            document.documentElement.style.setProperty('--top-begin', action.offsetTop  + 'px');
+            document.documentElement.style.setProperty('--height-end', (this.offsetHeight +4) + 'px');
+            document.documentElement.style.setProperty('--top-end', (this.offsetTop -4)  + 'px');
+            action.classList.remove('runanimation');
+            void action.offsetWidth;
+            action.classList.add('runanimation');
+        },false)
+    });
+
+
+
+
+
+// let nextBackground = 0;
+
+
+//     setInterval(() => {
+//         nextBackground +=1;
+//         if(nextBackground ===1) document.querySelector(".container").style.backgroundImage = "linear-gradient(#120d3d52, #080a2343), url('/images/img1.jpg')";
+//         if(nextBackground ===2) document.querySelector(".container").style.backgroundImage = "linear-gradient(#120d3d52, #080a2343), url('/images/img2.jpg')";
+//         if(nextBackground ===3) {
+//             nextBackground = 0 ; document.querySelector(".container").style.backgroundImage = "linear-gradient(#120d3d52, #080a2343), url('/images/bg.jpeg')";
+//         }
+        
+       
+//     }, 10000);
+
+const sections = document.querySelectorAll('section');
+
+function checkSections() {
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 150 && rect.bottom >= 150) {
+      section.classList.add('animate');
+    } else {
+      section.classList.remove('animate');
+    }
+  });
+  requestAnimationFrame(checkSections);
+}
+
+checkSections();
